8ed1df10209bfce78e4e6768d6e207b1da0f41fd
suddy <suddymail@gmail.com>
2011-10-10 16:14:35 +0800

    Init Project

---
+++ ios/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ios
+//
+//  Created by Songying Lin on 11-10-10.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
